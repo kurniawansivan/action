78eb59bb2f81af11380bcc27acb867f3166147dc
Ivan Kurniawan S <kurniawansivan@gmail.com>
2024-07-17 14:33:45 +0800

    Securely handle credentials without logging them

--- .github/scripts/upload_to_drive.py
+++ .github/scripts/upload_to_drive.py
@@ -6,8 +6,17 @@ import json
 
 def upload_to_drive(file_path, credentials_info, folder_id):
     SCOPES = ['https://www.googleapis.com/auth/drive.file']
+    
+    try:
+        credentials_data = json.loads(credentials_info)
+    except json.JSONDecodeError as e:
+        print("Failed to load JSON from credentials info.")
+        print(f"Error: {e}")
+        print(f"Credentials info: {credentials_info}")
+        raise
+    
     credentials = service_account.Credentials.from_service_account_info(
-        json.loads(credentials_info), scopes=SCOPES)
+        credentials_data, scopes=SCOPES)
 
     service = build('drive', 'v3', credentials=credentials)
 
@@ -26,5 +35,10 @@ if __name__ == "__main__":
     file_path = os.getenv("APK_PATH")
     credentials_info = os.getenv("GCP_CREDENTIALS_JSON")
     folder_id = os.getenv("GCP_FOLDER_ID")
+    
+    if not credentials_info:
+        print("GCP_CREDENTIALS_JSON environment variable is not set.")
+        raise ValueError("GCP_CREDENTIALS_JSON is not set.")
+    
     uploaded_file = upload_to_drive(file_path, credentials_info, folder_id)
     print(f'File ID: {uploaded_file.get("id")}')
